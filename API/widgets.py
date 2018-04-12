1920b5fd8dcf7da662455fee0920ca805669cab4
Александр Киняшов <kinyashov@gmail.com>
2018-04-12 17:58:26 +1000

    reduced __init__

--- API/widgets.py
+++ API/widgets.py
@@ -6,17 +6,15 @@ from django.template.loader import render_to_string
 class RangeSliderWidget(forms.Widget):
     template_name = 'slider.html'
 
-    def __init__(self, name_html_elements=None, attrs=None):
+    def __init__(self, id_html_elements=None, attrs=None):
         super().__init__(attrs)
-        self.name_slider = name_html_elements['slider_name']
-        self.min_input = name_html_elements['min_input']
-        self.max_input = name_html_elements['max_input']
+        self.id_html_elements = id_html_elements
 
     def render(self, name, value, attrs=None):
         super().render(name, value, attrs)
         context = {
-            'name_slider': self.name_slider,
-            'min_slider_input': self.min_input,
-            'max_slider_input': self.max_input
+            'slider_name': self.id_html_elements['slider_name'],
+            'min_slider_input': self.id_html_elements['min_input'],
+            'max_slider_input': self.id_html_elements['max_input']
         }
         return mark_safe(render_to_string(self.template_name, context))
