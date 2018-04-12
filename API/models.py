c26639394bb7c4597c5b92b1e1a6bb30a8de21b2
Александр Киняшов <kinyashov@gmail.com>
2018-04-12 17:09:58 +1000

    new parent class for range, remade class Stair

--- API/models.py
+++ API/models.py
@@ -1,30 +1,15 @@
 from django.db import models
 
+from .fields import SliderField
+
 
 class Stair(models.Model):
     title = models.CharField('Название лестницы', max_length=100)
-    min_height = models.PositiveIntegerField('Минимальная высота лестницы', default=600)
-    max_height = models.PositiveIntegerField('Максимальная высота лестницы', default=3600)
-    step_height = models.PositiveSmallIntegerField('Шаг высоты лестницы', default=5)
-    min_marsh = models.PositiveIntegerField('Минимальная ширина марша', default=550)
-    max_marsh = models.PositiveIntegerField('Максимальная ширина марша', default=1300)
-    step_marsh = models.PositiveSmallIntegerField('Шаг ширины марша', default=5)
     type_turn = models.ManyToManyField('Turn', blank=True, verbose_name='Варианты поворота')
-    min_width_step = models.PositiveIntegerField('Минимальная ширина ступени', default=100)
-    max_width_step = models.PositiveIntegerField('Максимальная ширина ступени', default=350)
-    step_width_step = models.PositiveSmallIntegerField('Шаг ширины ступени', default=5)
-    min_width_top_step = models.PositiveIntegerField('Минимальная ширина верхней ступени', default=40)
-    max_width_top_step = models.PositiveIntegerField('Максимальная ширина верхней ступени', default=300)
-    step_width_top_step = models.PositiveSmallIntegerField('Шаг ширины верхней ступени', default=5)
-    visible_first_marsh = models.BooleanField('Ограждение на первом марше', default=True)
-    visible_invitation_step = models.BooleanField('Пригласительная ступень', default=True)
     type_fence = models.ManyToManyField('Fence', blank=True, verbose_name='Варианты ограждений')
-    visible_riser = models.BooleanField('Подступенок', default=True)
     type_track_support = models.ManyToManyField('TrackSupport', blank=True, verbose_name='Варианты пристенного профиля')
     material_stair = models.ManyToManyField('Material', blank=True, verbose_name='Доступные материалы')
     tint = models.ManyToManyField('Tint', blank=True, verbose_name='Варианты отделки')
-    patina = models.BooleanField('Патина', default=True)
-    brush = models.BooleanField('Браш', default=True)
 
     class Meta:
         verbose_name = 'Лестница'
@@ -32,6 +17,33 @@ class Stair(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Range(models.Model):
+    title = models.CharField(max_length=16)
+    min = models.PositiveIntegerField(default=0)
+    slider = SliderField()
+    max = models.PositiveIntegerField(default=10000)
+    step = models.PositiveSmallIntegerField()
+
+    class Meta:
+        abstract = True
+
+
+class HeightRange(Range):
+    stair = models.OneToOneField('Stair', on_delete=models.CASCADE)
+
+
+class WidthMarsh(Range):
+    stair = models.OneToOneField('Stair', on_delete=models.CASCADE)
+
+
+class MinWidthStep(Range):
+    stair = models.OneToOneField('Stair', on_delete=models.CASCADE)
+
+
+class MinWidthTopStep(Range):
+    stair = models.OneToOneField('Stair', on_delete=models.CASCADE)
 
 
 class Turn(models.Model):
