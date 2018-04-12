694281fae74346d6b43643b30bba4103c5566baf
Александр Киняшов <kinyashov@gmail.com>
2018-04-12 17:04:03 +1000

    added new classes with slider

--- API/admin.py
+++ API/admin.py
@@ -2,49 +2,32 @@ from django.contrib import admin
 from django.db import models
 from django import forms
 
-from .models import Stair, Turn, Fence, TrackSupport, Material, Tint
-from .forms import StairForm
+from .models import (
+    Stair, Turn, Fence, TrackSupport, Material, Tint,
+    HeightRange, WidthMarsh, MinWidthStep, MinWidthTopStep
+)
+
+
+class HeightRangeInline(admin.TabularInline):
+    model = HeightRange
+
+
+class WidthMarshInline(admin.TabularInline):
+    model = WidthMarsh
+
+
+class MinWidthStepInline(admin.TabularInline):
+    model = MinWidthStep
+
+
+class MinWidthTopStepInline(admin.TabularInline):
+    model = MinWidthTopStep
 
 
 class StairAdmin(admin.ModelAdmin):
-
-    form = StairForm
-    fieldsets = (
-        (None, {
-            'fields': ('title',)
-        }),
-        ('Высота лестницы', {
-            'fields': (('min_height', 'max_height', 'step_height',), 'height')
-        }),
-        ('Ширина марша', {
-            'fields': (('min_marsh', 'max_marsh', 'step_marsh'), 'marsh')
-        }),
-        ('Какие типы поворотов доступны', {
-            'fields': ('type_turn',)
-        }),
-        ('Ширина ступеней', {
-            'fields': (('min_width_step', 'max_width_step', 'step_width_step'), 'width_step',)
-        }),
-        ('Ширина верхней ступени', {
-            'fields': (('min_width_top_step', 'max_width_top_step', 'step_width_top_step'), 'width_top_step',)
-        }),
-        ('Какие типы ограждений доступны', {
-            'fields': ('type_fence',)
-        }),
-        ('Какие типы пристенного профиля доступны', {
-            'fields': ('type_track_support',)
-        }),
-        ('Какие типы отделки доступны', {
-            'fields': ('tint',)
-        }),
-        ('Какие материалы доступны', {
-            'fields': ('material_stair',)
-        }),
-        ('Какие дополнительные элементы доступны', {
-            'fields': ('visible_first_marsh', 'visible_invitation_step', 'visible_riser', 'patina', 'brush')
-        }),
-    )
-
+    inlines = [
+        HeightRangeInline, WidthMarshInline, MinWidthStepInline, MinWidthTopStepInline
+    ]
     formfield_overrides = {
         models.ManyToManyField: {'widget': forms.CheckboxSelectMultiple},
     }
@@ -56,3 +39,7 @@ admin.site.register(Fence)
 admin.site.register(TrackSupport)
 admin.site.register(Material)
 admin.site.register(Tint)
+admin.site.register(HeightRange)
+admin.site.register(WidthMarsh)
+admin.site.register(MinWidthStep)
+admin.site.register(MinWidthTopStep)
