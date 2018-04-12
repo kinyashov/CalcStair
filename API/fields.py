c82fc87341b7d93340660912085657e53d682bf0
Александр Киняшов <kinyashov@gmail.com>
2018-04-12 17:07:01 +1000

    new file with logic of slider-field, where generic id for HTML elements

---
+++ API/fields.py
@@ -0,0 +1,23 @@
+from django.db.models.fields import NullBooleanField
+
+from .forms import SliderFormField
+
+
+class SliderField(NullBooleanField):
+    empty_strings_allowed = False
+
+    @property
+    def id_html_elements(self):
+        model_name = self.model._meta.model_name
+        slider_name = f"id_{model_name}"
+        min_input = f"{model_name}-0-min"
+        max_input = f"{model_name}-0-max"
+        return slider_name, min_input, max_input
+
+    def formfield(self):
+        slider_name, min_input, max_input = self.id_html_elements
+        defaults = {'form_class': SliderFormField,
+                    'slider_name': slider_name,
+                    'min_input': min_input,
+                    'max_input': max_input}
+        return super().formfield(**defaults)
