8aca484a5299c59cd6655e0ce85df0c3463e164f
kinyashov <kinyashov@gmail.com>
2018-04-28 18:06:36 +1000

    optimized serializers

--- API/views.py
+++ API/views.py
@@ -1,25 +1,13 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from .models import Stair
 from .serializers import StairSerializer
+from rest_framework import generics
 
 
-@csrf_exempt
-def stair_list(request):
-
-    if request.method == 'GET':
-        snippets = Stair.objects.all()
-        serializer = StairSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, safe=False)
+class StairList(generics.ListCreateAPIView):
+    queryset = Stair.objects.all()
+    serializer_class = StairSerializer
 
 
-@csrf_exempt
-def stair_detail(request, pk):
-    try:
-        snippet = Stair.objects.get(pk=pk)
-    except Stair.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = StairSerializer(snippet)
-        return JsonResponse(serializer.data)
+class StairDetail(generics.RetrieveAPIView):
+    queryset = Stair.objects.all()
+    serializer_class = StairSerializer
