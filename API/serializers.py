d05aeafd53ae81b40bb21755179602ffc6e4f5bd
kinyashov <kinyashov@gmail.com>
2018-04-28 18:04:01 +1000

    added HeightSerializer

--- API/serializers.py
+++ API/serializers.py
@@ -3,7 +3,15 @@ from rest_framework import serializers
 from .models import *
 
 
+class HeightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Height
+        fields = ('min', 'max', 'step')
+
+
 class StairSerializer(serializers.ModelSerializer):
+    height = HeightSerializer
+
     class Meta:
         model = Stair
-        fields = "__all__"
+        fields = ('title', 'height')
