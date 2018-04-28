5d5c51c59b1eac8c2851028f81d1bc34f88cfca2
kinyashov <kinyashov@gmail.com>
2018-04-28 18:05:48 +1000

    minor changes

--- API/urls.py
+++ API/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^API/$', views.stair_list),
-    url(r'^API/(?P<pk>[0-9]+)/$', views.stair_detail),
+    url(r'^API/$', views.StairList.as_view()),
+    url(r'^API/(?P<pk>[0-9]+)/$', views.StairDetail.as_view()),
 ]
