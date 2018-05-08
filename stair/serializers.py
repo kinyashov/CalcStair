617d2f8aabd7b49fb85f3c76fa7b621aff7c5082
kinyashov <kinyashov@gmail.com>
2018-05-08 21:10:23 +1000

    added serializers for material, fence, tint and track support

--- stair/serializers.py
+++ stair/serializers.py
@@ -3,6 +3,30 @@ from rest_framework import serializers
 from .models import *
 
 
+class MaterialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Material
+        fields = '__all__'
+
+
+class FenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Fence
+        fields = '__all__'
+
+
+class TrackSupportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrackSupport
+        fields = '__all__'
+
+
+class TintSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tint
+        fields = '__all__'
+
+
 class HeightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Height
@@ -32,6 +56,10 @@ class StairSerializer(serializers.ModelSerializer):
     width_marsh = WidthMarshSerializer()
     min_width_step = MinWidthStepSerializer()
     min_width_top_step = MinWidthTopStepSerializer()
+    fences = FenceSerializer(many=True)
+    track_supports = TrackSupportSerializer(many=True)
+    materials = MaterialSerializer(many=True)
+    tints = TintSerializer(many=True)
 
     class Meta:
         model = Stair
@@ -39,4 +67,8 @@ class StairSerializer(serializers.ModelSerializer):
                   'height',
                   'width_marsh',
                   'min_width_step',
-                  'min_width_top_step')
+                  'min_width_top_step',
+                  'fences',
+                  'track_supports',
+                  'materials',
+                  'tints')
